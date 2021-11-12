abf7bbd1b398e24f0070930214e274f64039a04d
Blocktt <68349347+Blocktt@users.noreply.github.com>
2021-11-12 08:57:36 -0500

    v0.1.1.9000
    Added first version of all R Scripts needed for R Shiny workshop.

---
+++ NAB_RShiny_Workshop/L01_Script02_InputExample.R
@@ -0,0 +1,15 @@
+# L01_Script02_InputExample
+library(shiny)
+ui <- fluidPage(
+  sliderInput(inputId = "num",
+              label = "Choose a number",
+              value = 25, min = 1, max = 100),
+  plotOutput("hist")
+)
+server <- function(input, output, session) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$num))
+  })
+}
+shinyApp(ui, server)
+
---
+++ NAB_RShiny_Workshop/L02_Script05_observeEvent.R
@@ -0,0 +1,13 @@
+#L02_Script05_observeEvent
+library(shiny)
+
+ui <- fluidPage(
+  actionButton(inputId = "clicks",
+               label = "Click me")
+)
+server <- function(input, output) {
+  observeEvent(input$clicks
+               , showNotification(input$clicks))
+}
+
+shinyApp(ui = ui, server = server)
